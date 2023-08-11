f2d36671422200bd1c323434ab2225c1f5765f56
IamGOOD <rukshenas1990@gmail.com>
2023-08-11 12:32:33 -0400

    Added pages directory

--- test_main_page.py
+++ test_main_page.py
@@ -3,6 +3,11 @@ from selenium.webdriver.common.by import By
 link = 'http://selenium1py.pythonanywhere.com/'
 
 
-def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
+def go_to_login_page(browser):
     browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+
+def test_quest_can_go_to_login_page(browser):
+    browser.get(link)
+    go_to_login_page(browser)
+
+

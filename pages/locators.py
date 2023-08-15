e4137cceaaee7a8fe7aa237890df8bda7c38ac7a
IamGOOD <rukshenas1990@gmail.com>
2023-08-15 10:32:09 -0400

    Fixed bugs; changed locators.py filds --> forms; implemented login link transition

--- pages/locators.py
+++ pages/locators.py
@@ -4,8 +4,5 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
 
 class LoginPageLocators():
-    LOGIN_EMAIL = (By.NAME, 'login-username') # [name= ]
-    LOGIN_PASSWORD = (By.NAME, 'login-password')
-    REGISTER_EMAIL = (By.NAME, 'registration-email')
-    REGISTER_PASSWORD = (By.NAME, 'registration-password1')
-    REGISTER_CONFIRM_PASSWORD = (By.NAME, 'registration-password2')
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,10 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
-from .login_page import LoginPage
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
         self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
-
 
 
     def should_be_login_link(self):

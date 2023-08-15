7e70fe4636e99eb8639b0ec92e8421cc89bf191a
IamGOOD <rukshenas1990@gmail.com>
2023-08-14 22:12:23 -0400

    Implementation of the transition between pages

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,8 +7,8 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self, browser):
-        assert 'login' in browser.current_url, '"login not found in current url'
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url, '"login not found in current url'
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL,

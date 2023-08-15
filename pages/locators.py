59a590478890dd0d713eb8004baa4f6635a41bcd
IamGOOD <rukshenas1990@gmail.com>
2023-08-14 21:35:27 -0400

    Added 'login_page' and test login and register forms

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,10 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+class LoginPageLocators():
+    LOGIN_EMAIL = (By.NAME, 'login-username') # [name= ]
+    LOGIN_PASSWORD = (By.NAME, 'login-password')
+    REGISTER_EMAIL = (By.NAME, 'registration-email')
+    REGISTER_PASSWORD = (By.NAME, 'registration-password1')
+    REGISTER_CONFIRM_PASSWORD = (By.NAME, 'registration-password2')
--- test_main_page.py
+++ test_main_page.py
@@ -14,3 +14,11 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+# def should_be_login_url(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/'
+#     page = MainPage(browser, link)
+#     page.open()
+
+
+

f0d9704e0debedd6bc463555d3e2f4ff3472ad28
IamGOOD <rukshenas1990@gmail.com>
2023-08-20 08:40:07 -0400

    1. Promotion tested, bug found in offer 7, bug marked as falling. 2. Found a bug in the code and fixed it

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,37 +3,41 @@ from.locators import ProductPageLocators
 
 class ProductPage(BasePage):
 
-    def add_to_basket(self):
-        self.get_product_price()
-        self.get_product_title()
-        self.should_be_basket_button()
-        self.click_basket_button()
+    def add_product_to_basket(self):
+        self.should_be_add_to_basket_button()
+        self.click_add_to_basket_button()
 
     def check_basket_on_added_product_and_total_basket(self):
         self.should_be_message_added_product_title_in_basket()
         self.should_be_correct_basket_total_after_added_product()
 
-
-    def should_be_basket_button(self):
+    def should_be_add_to_basket_button(self):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), 'basket button not found'
 
-    def click_basket_button(self):
+    def click_add_to_basket_button(self):
         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
 
-    def get_product_title(self):
-        assert self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE), 'product title not found'
-        product_title = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
-        return product_title
+    def get_product_title_on_page(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_TITLE), 'product title in store not found'
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
 
-    def get_product_price(self):
-        assert self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE), 'product price not found'
-        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
-        return product_price
+    def get_product_price_on_page(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), 'product price in store not found'
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+
+    def get_product_title_in_basket(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_ADDED_MASSAGE), 'product title not found in basket'
+        return self.browser.find_element(*ProductPageLocators.BASKET_ADDED_MASSAGE).text
+
+    def get_basket_total(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_TOTAL_PRICE), 'basket total not found'
+        return self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_PRICE).text
 
     def should_be_message_added_product_title_in_basket(self):
-        assert self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text \
-               in self.get_product_title(), "The product wasn't added to basket" # self.browser.find_element(*ProductPageLocators.BASKET_ADDED_MASSAGE)
+        assert self.get_product_title_on_page() == self.get_product_title_in_basket(), "product wasn't added to basket"
 
     def should_be_correct_basket_total_after_added_product(self):
-        assert self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text \
-               == self.get_product_price(), 'The price of product != total of basket'# self.browser.find_element
+        assert self.get_product_price_on_page() == self.get_basket_total(), 'The price of product != total of basket'
+
+
+
